3cb796c9d8aad994c436af447af4afb58e421c4f
santhosh <santhoshpanneer03@gmail.com>
2026-04-08 21:32:08 +0530

    canteen management system

--- Smart Canteen Management/DBConnection.java
+++ Smart Canteen Management/DBConnection.java
@@ -5,8 +5,8 @@ public class DBConnection {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             // Updated with your server IP and credentials
-            String url = "jdbc:sqlserver://your server ip:1433;databaseName=DBname;encrypt=true;trustServerCertificate=true";
-            Connection con = DriverManager.getConnection(url, "Username", "Password");
+            String url = "jdbc:sqlserver://server IP:1433;databaseName=DBname;encrypt=true;trustServerCertificate=true";
+            Connection con = DriverManager.getConnection(url, "username", "password");
             return con;
         } catch (Exception e) {
             System.out.println("Database Connection Error: " + e);
